9caee5a83e5d041f45ab5cf30ec0bace4a7afb20
andrea <andrea@klettke.nl>
2021-02-18 21:39:35 +0100

    fixed edit post error

--- blog/views.py
+++ blog/views.py
@@ -40,7 +40,8 @@ def post_edit(request, pk):
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
-        form = PostForm(request.POST, instance=post)
+        form = PostForm(instance=post)
+        # form = PostForm(request.POST, instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def show_picture(request):
